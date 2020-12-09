e8579ba12d058e3589a7ba114fe11ee44816561d
Punit Katiyar <65483081+codewithpunit@users.noreply.github.com>
2020-12-09 21:00:29 +0530

    Add files via upload

---
+++ control.php
@@ -0,0 +1,60 @@
+<?php
+//control statement
+/*
+= assignment Oprater
+== comparision
+&& and 
+|| or
+!! not
+< less
+> greater
+*/
+$data=40;
+if($data==1)
+{
+    echo "ONE";
+}
+else if($data==2)
+{
+    echo "TWO";
+}
+else if($data==3)
+{
+    echo "THREE";
+}
+else
+{
+    echo "condition not match";
+}
+// A E I O U
+echo '<hr>';
+$char="a";
+if($char=="a" or $char=='e' or $char=='i' or $char=='o' or $char=='u' )
+{
+    echo "Vowel";
+}
+else 
+{
+    echo "Consonent";
+}
+echo '<hr>';
+// less and greater
+if($data >= 41)
+{
+    echo "ok";
+}
+else 
+{
+    echo "Not Ok";
+}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ switch.php
@@ -0,0 +1,22 @@
+<?php
+$data=20;
+switch($data)
+{
+    case 1:
+        echo "one";
+    break;
+    case 2:
+        echo "Two";
+    break;
+    case 10:
+        echo "TEN";
+    break;
+    case 11:
+        echo "Eleven";
+    break;
+    default :
+        echo "bye";
+}
+
+
+?>
