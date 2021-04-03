f8504db4628c5ab34508d651eff2ebef827b3ecf
Code With Punit <65483081+codewithpunit@users.noreply.github.com>
2021-04-03 13:30:22 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,41 @@
+<title>Variable</title>
+<?php
+//define the variable
+$data="Hello";
+echo $data;
+$num=10;
+echo $num;
+/*
+constant 
+$height=px;
+echo $height;
+*/
+$DATA=100;
+echo $DATA;
+$myacccountnumber=101;
+echo '<h1>';
+echo $myacccountnumber;
+echo '</h1>';
+//$my acccount number=101;
+//$my-account-number=101;
+$my_account_number=101;
+$myAccountNumber=101;
+//$1box="100%";
+$box1="100%";
+echo $box1;
+//$__data=100;
+//echo $__data;
+$_GET[];
+$_POST[];
+$_REQUEST[];
+$_FILES[];
+$_SESSION[];
+$_COOKIE[];
+$_SERVER[];
+
+
+
+
+
+
+?>
